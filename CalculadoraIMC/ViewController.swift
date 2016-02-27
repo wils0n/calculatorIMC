232e374e01c0a20ff96773fedc37f92eee63c25d
wils0n <wiljm0.0@gmail.com>
2016-02-27 18:27:11 -0500

    Initial Commit

---
+++ CalculadoraIMC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculadoraIMC
+//
+//  Created by Wilson Mejía on 27/02/16.
+//  Copyright © 2016 NinjaLABS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
