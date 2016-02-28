801b6602f62a316f8bc2ad2c8adbef8a7c9a8ce1
wils0n <wiljm0.0@gmail.com>
2016-02-27 20:56:10 -0500

    adding funcionality

--- CalculadoraIMC/ViewController.swift
+++ CalculadoraIMC/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var peso: UITextField!
+    @IBOutlet weak var estatura: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func calcularIMC(sender: AnyObject) {
+        var IMC:Double;
+        let pesoLocal:Double = Double(self.peso.text!)!;
+        let estaturaLocal:Double = Double(self.estatura.text!)!;
+        
+        IMC = pesoLocal/(estaturaLocal*estaturaLocal)
+        print("Resultado:\(IMC)")
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
