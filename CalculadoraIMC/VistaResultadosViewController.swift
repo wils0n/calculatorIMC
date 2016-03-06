a376f9ceb18684fd61f0658101fe7ddf0dd8b7d4
wils0n <wiljm0.0@gmail.com>
2016-03-06 11:16:12 -0500

    new view for result

---
+++ CalculadoraIMC/VistaResultadosViewController.swift
@@ -0,0 +1,36 @@
+//
+//  VistaResultadosViewController.swift
+//  CalculadoraIMC
+//
+//  Created by Wilson Mejía on 6/03/16.
+//  Copyright © 2016 NinjaLABS. All rights reserved.
+//
+
+import UIKit
+
+class VistaResultadosViewController: UIViewController {
+
+    @IBOutlet weak var resultadoIMC: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
